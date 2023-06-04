51b4b2bbc1990ffd58fafffde7f03b564a782a31
applh.com <contact@applh.com>
2023-06-04 16:09:42 +0200

    v0.9.7 (docker) laravel + gaia mix

--- docker/container-laravel/app/routes/web.php
+++ docker/container-laravel/app/routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// add fallback
+// WARNING: must be the last route
+Route::fallback(function () {
+    $env_gaia_path = env('GAIA_PATH');
+    // laravel base path
+    $laravel_path = base_path();
+    $path_gaia = realpath("$laravel_path/$env_gaia_path");
+
+    ob_start();
+    include $path_gaia;
+    $code = ob_get_clean();
+
+    return $code;
+
+});
