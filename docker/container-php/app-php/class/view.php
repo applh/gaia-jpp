5e742736d65b5f67ea2fc9813bcc8cc15923d7ff
applh.com <contact@applh.com>
2023-08-23 16:45:57 +0200

    v1.6.6 (docker) add volume /var/code for PHP * speed is about the same as using container local /tmp

--- docker/container-php/app-php/class/view.php
+++ docker/container-php/app-php/class/view.php
@@ -66,6 +66,94 @@ class view
 
     }
 
+    static function read_inc ($loop_template = null) 
+    {
+        model::read();
+        $rows = response::$rows ?? [];
+
+        $html_bloc = "";
+
+        // test switch to compare performances
+        $skip_template = FALSE;
+
+        if (!$skip_template) {
+            $loop_template ??= "loop-item";
+            // $tmp_dir = sys_get_temp_dir();
+            $tmp_dir = "/var/code";
+
+            // get the code from template parts/loop-item.php
+            $code_item = file_get_contents(__DIR__ . "/../templates/parts/$loop_template.php");
+            
+            // create a tmp file in the code directory
+            if (is_writable($tmp_dir)) {
+                $tmp_file = "$tmp_dir/$loop_template.php";
+                // check if file exists
+                if (!file_exists($tmp_file)) {
+                    // copy the code to a temporary file
+                    file_put_contents($tmp_file, $code_item);
+                }
+                else {
+                    // check if file is different
+                    $code_tmp = file_get_contents($tmp_file);
+                    if ($code_tmp != $code_item) {
+                        // copy the code to a temporary file
+                        file_put_contents($tmp_file, $code_item);
+                    }
+                }
+            }
+            else {
+                $tmp_file = tempnam(sys_get_temp_dir(), "item");
+                // copy the code to a temporary file
+                file_put_contents($tmp_file, $code_item);
+            }
+
+    
+            // $tmp_file = __DIR__ . "/../templates/parts/$loop_template.php";
+
+            $html_bloc .= 
+            <<<HTML
+            <div class="w100 text-center">
+                <p>loop_template: $loop_template</p>
+                <p>tmp_dir: $tmp_dir</p>
+                <p>tmp_file: $tmp_file</p>
+            </div>
+            HTML;
+        }
+
+        foreach($rows as $index => $row) {
+
+            // WARNING: VERY SLOW WITH DOCKER 😱
+            // maybe looping and including files is not a good idea...
+            // build html template item
+            $html_item = view::load_template($tmp_file ?? "", [
+                "skip" => $skip_template,
+                "row" => $row,
+                "index" => $index,
+            ]);
+
+
+            // OK: about same performance as json output
+            if ($skip_template) {
+                $html_item = 
+                <<<HTML
+                <section class="s3">
+                    <h3>{$row["name"]}</h3>
+                    <img src="/photo.jpg" alt="">
+                    <p>{$index}, {$row["id"]}, {$row["email"]}</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam velit, magni odio, suscipit corrupti dicta totam deleniti sed adipisci necessitatibus sapiente dolorum optio eius nemo rerum. Maiores facere laboriosam perspiciatis.</p>
+                </section>
+                HTML;                
+            }
+
+            // add item to bloc
+            $html_bloc .= $html_item ?? "";
+        }
+
+        // build html template
+        echo $html_bloc;
+
+    }
+
     static function load_template($template, $params = [])
     {
         // warning: create local variables from array keys
---
+++ docker/container-php/app-php/templates/news-inc.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="/">home</a></li>
+                <li><a href="/news">news</a></li>
+                <li><a href="/app">app</a></li>
+                <li><a href="/admin">admin</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>NEWS</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur at tempora mollitia labore sequi libero quam aspernatur quos nesciunt adipisci aperiam dignissimos possimus, excepturi, et dicta veritatis enim ipsum consequuntur.</p>
+        <section class="s2">
+            <h2>latest news</h2>
+            <?php view::read_inc() ?>
+        </section>
+    </main>
+    <footer>
+        <p>all rights reserved / 2023</p>
+    </footer>
+
+    <script type="module" src="/site.js"></script>
+</body>
+</html>
